e17c0bb10b348f2e728866566dd0d0533309e385
acfish <605907914@qq.com>
2013-05-08 20:16:50 +0800

    Update poj 1026.cpp

--- poj 1026.cpp
+++ poj 1026.cpp
@@ -17,26 +17,32 @@ vector<node> ee[210];
 void init()
 {
     memset(mark,0,sizeof(mark));
+    memset(w,0,sizeof(w));
+    memset(getans,0,sizeof(getans));
     for(int ii=0;ii<=200;ii++)
     ee[ii].clear();
     return ;
 }
 int main()
 {
-    int i,j,m,n;
-    while(scanf("%d",&n),n)
-    {
-        for(i=1;i<=n;i++)
+        int i,j,m,n;
+        while(scanf("%d",&n),n)
         {
+          for(i=1;i<=n;i++)
+          {
             scanf("%d",&s[i].num);
             s[i].sta=i;
-        }
-        scanf("%d",&m);
-        getchar();
-        gets(w);
-        int top=0;
-        for(i=1;i<=n;i++)
-        {
+          }
+          while(1)
+          {
+          init();
+          scanf("%d",&m);
+          if(m==0)break;
+          getchar();
+          gets(w);
+          int top=0;
+          for(i=1;i<=n;i++)
+          {
             while(!mark[i])
             {
                 ee[top].push_back(s[i]);
@@ -46,22 +52,23 @@ int main()
             int nn=ee[top].size();
             for(j=0;j<nn;j++)
             {
-                ans[ee[top][j].sta]=ee[top][(j+m)%nn].num;
+                ans[ee[top][j].sta]=ee[top][(j+m-1)%nn].num;
             }
             top++;
-        }
-        for(i=1;i<=n;i++)
-        cout<<ans[i]<<endl;
-        int mm=strlen(w);
-        for(i=1;i<=n;i++)
-        {
-            if(n-1>mm)
+          }
+          int mm=strlen(w);
+          for(i=1;i<=n;i++)
+          {
+            if(i>mm)
             getans[ans[i]-1]=' ';
             else
-            getans[ans[i]-1]=w[ans[i]-1];
+            getans[ans[i]-1]=w[i-1];
+          }
+           for(i=0;i<n;i++)
+            printf("%c",getans[i]);
+            printf("\n");
+          }
+         printf("\n");
         }
-        puts(getans);
-        printf("\n\n");
-    }
     return 0;
 }
